2c402b1b7bf0dc98d364c093da60f2167745a655
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-05-18 17:20:56 +0200

    springboot application starter

---
+++ FutureFeed/src/main/java/com/syntexsquad/FutureFeed/FutureFeedApplication.java
@@ -0,0 +1,13 @@
+package com.syntexsquad.FutureFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureFeedApplication.class, args);
+	}
+
+}
---
+++ FutureFeed/src/test/java/com/syntexsquad/FutureFeed/FutureFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntexsquad.FutureFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
