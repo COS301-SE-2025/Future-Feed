e7b6a2ba4db896bd2e8de896cdebe8ce6bdd4205
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-05-23 00:33:58 +0200

    updated Appuser
    added more fields to appuser

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/AppUser.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/AppUser.java
@@ -38,10 +38,63 @@ public class AppUser {
 
     private LocalDate dateOfBirth;
 
+    private String bio;
+
+
     public AppUser(String username, String password) {
         this.username = username;
         this.password = password;
         this.role = "USER";
     }
+
+   public String  getUsername() {
+        return username;
+    }
+   public  void setUsername(String username) {
+        this.username = username;
+    }
+   public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   public String getPassword() {
+        return password;
+    }
+   public  void setPassword(String password) {
+        this.password = password;
+    }
+   public String getRole() {
+        return role;
+    }
+   public  void setRole(String role) {
+        this.role = role;
+    }
+   public String getDisplayName() {
+        return displayName;
+    }
+   public  void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+   public String getProfilePicture() {
+        return profilePicture;
+    }
+public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+   public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+   public  void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+   public String getBio() {
+        return bio;
+    }
+   public  void setBio(String bio) {
+        this.bio = bio;
+    }
+
 }
 
