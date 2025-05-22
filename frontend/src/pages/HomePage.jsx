fc059b9b4039766423a566a6642366c6a369ffd8
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-05-22 17:51:01 +0200

    Updates on the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,143 +1,177 @@
 import React from 'react';
 import { useState } from 'react';
 import '../styles/homePage.css';
-import { FaHome, FaSearch, FaHeart, FaRobot, FaUser, FaBell} from 'react-icons/fa';
+import ffCropped from "../assets/FF cropped.png";
+import { FaHome, FaSearch, FaHeart, FaRobot, FaUser, FaBell, FaEllipsisH, FaComment, FaRetweet, FaStar } from 'react-icons/fa';
 
 const HomePage = () => {
     const [postText, setPostText] = useState('');
     return (
-        
-
-        // <div className="app-container">
-        //     {/* Navigation Sidebar */}
-        //     <div className="navigation-sidebar">
-        //         <nav>
-        //             <ul>
-        //                 <li className="active"><FaHome className="icon" /> <span>Home</span></li>
-        //                 <li><FaSearch className="icon" /> <span>Search</span></li>
-        //                 <li><FaRobot className="icon" /> <span>Bots</span></li>
-        //                 <li className="icon"> Addition</li>
-        //                 <li><FaHeart className="icon" /> <span>Activity</span></li>
-        //                 <li><FaUser className="icon" /> <span>Profile</span></li>
-        //             </ul>
-        //         </nav>
-        //     </div>
-
-        //     {/* Main Feed */}
-        //     <div className="main-feed">
-        //         {/* Create Post */}
-        //         <div className="create-post">
-        //             <h3>What's on your mind?</h3>
-        //             <button className="post-button">Post</button>
-        //         </div>
-
-        //         {/* Posts Feed */}
-        //         <div className="posts-container">
-        //             <div className="post">
-        //                 <div className="post-header">
-        //                     <span className="username">Display Name</span>
-        //                     <span className="post-time">2 days ago</span>
-        //                 </div>
-        //                 <div className="post-content">
-        //                     <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
-        //                 </div>
-        //             </div>
-
-        //             <div className="post">
-        //                 <div className="post-header">
-        //                     <span className="username">Bot</span>
-        //                     <span className="post-time">1hr ago</span>
-        //                 </div>
-        //                 <div className="post-content">
-        //                     <p>+ Active</p>
-        //                     <p>Brand New Audi RS 5 ABT SportsBack, What a boost!!!</p>
-        //                 </div>
-        //             </div>
-
-        //             <div className="post">
-        //                 <div className="post-header">
-        //                     <span className="username">Display Name</span>
-        //                     <span className="post-time">3 days ago</span>
-        //                 </div>
-        //                 <div className="post-content">
-        //                     {/* Post content would go here */}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className = "home-page">
-            {/*sidebar*/}
-            <div className ="navigation-sidebar">
+        <div className="home-page">
+            {/* Logo */}
+            <img src={ffCropped} alt="Future Feed Logo" className="homePage-logo" />
+            {/* Sidebar */}
+            <div className="navigation-sidebar">
                 <nav>
-                     <ul>
-                         <li className="active"><FaHome className="icon" /> </li>
-                         <li><FaSearch className="icon" /> </li>
-                         <li><FaRobot className="icon" /> </li>
-                         <li><FaBell className="icon"/> </li>
-                         <li><FaHeart className="icon" /> </li>
-                         <li><FaUser className="icon" /> </li>
-                     </ul>
-                 </nav>
+                    <ul>
+                        <li className="active"><FaHome className="icon" /></li>
+                        <li><FaSearch className="icon" /></li>
+                        <li><FaRobot className="icon" /></li>
+                        <li><FaBell className="icon" /></li>
+                        <li><FaHeart className="icon" /></li>
+                        <li><FaUser className="icon" /></li>
+                    </ul>
+                </nav>
             </div>
-            {/*sidebar*/}
-            <h1 className='welcome-message'>Welcome, Feature feed</h1>
-            <div className ="feed">
-                    <div className="new-post">
+            {/* Feed */}
+            <div className="feed">
+                <div className="new-post">
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log('Posted:', postText);
+                        setPostText('');
+                    }}>
                         <textarea
                             className="new-post-text"
                             value={postText}
                             onChange={(e) => setPostText(e.target.value)}
                             placeholder="What's on your mind?"
-                            rows="2" // Adjust for desired height
-                            />
-                        <div className="create-post">
-                            <button className="create-post-text"> New Post</button>
-                        </div>
+                            rows="2"
+                        />
+                        <button type="submit" className="create-post">
+                            <h3 className="create-post-text">New Post</h3>
+                        </button>
+                    </form>
+                </div>
+                <div className="main-seperator"></div>
+                {/* Post 1 */}
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
                     </div>
-
-                    <div className='main-seperator'></div>
-
-                    <div className="post">
-                         
-                         <div className="post-header">
-                             <span className="username">Display Name</span>
-                             <span className="seperator">|</span>
-                             <span className="post-time">2 days ago</span>
-                         </div>
-                         <div className="post-content">
-                             <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
-                         </div>
-                     </div>
-
-                     <div className='main-seperator'></div>
-
-                     <div className="post">
-                         <div className="post-header">
-                             <span className="username">Bot</span>
-                             <span className="seperator">|</span>
-                             <span className="post-time">1hr ago</span>
-                         </div>
-                         <div className="post-content">
-                             <p>+ Active</p>
-                             <p>Brand New Audi RS 5 ABT SportsBack, What a boost!!!</p>
-                         </div>
-                     </div>
-
-                     <div className='main-seperator'></div>
-
-                     <div className="post">
-                         <div className="post-header">
-                             <span className="username">Display Name</span>
-                             <span className="seperator">|</span>
-                             <span className="post-time">3 days ago</span>
-                         </div>
-                         <div className="post-content">
-                             {/* Post content would go here */}
-                         </div>
-                     </div>
-                     <div className='main-seperator'></div>
-                     
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
+                    </div>
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
+                    </div>
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
+                    </div>
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
+                    </div>
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
+                {/* Post 2 */}
+                <div className="post">
+                    <div className="post-header">
+                        <img src={ffCropped} alt="" className="profile-image" />
+                        <span className="username">Display Name</span>
+                        <span className="seperator">|</span>
+                        <span className="post-time">2 days ago</span>
+                        <span className="settings"><FaEllipsisH className="icon" /></span>
+                    </div>
+                    <div className="post-content">
+                        <p>Hey everyone, I hope all is well. I'd like to take this opportunity to introduce you to future feed</p>
+                    </div>
+                    <div className="post-actions">
+                        <div className="left-actions">
+                            <button className="action-button"><FaHeart className="action-icon" /> </button>
+                            <button className="action-button"><FaComment className="action-icon" /> </button>
+                            <button className="action-button"><FaRetweet className="action-icon" /> </button>
+                        </div>
+                        <button className="bookmark-button"><FaStar className="action-icon" /> </button>
+                    </div>
+                </div>
+                <div className="main-seperator"></div>
             </div>
         </div>
     );
