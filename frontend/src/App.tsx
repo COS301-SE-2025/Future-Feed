96f285594869567848ddcfd788dd6fe48cccfa60
Lesedi <u22564812@tuks.co.za>
2025-06-07 21:50:30 +0200

    Added route for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
 import UserProfile from "./pages/UserProfile"
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Routes>
       {/* Future routes: <Route path="/" element={<Landing />} /> */}
+      <Route path="/" element={<LandingPage />} />
       <Route path="/profile" element={<UserProfile />} />
     </Routes>
   )
