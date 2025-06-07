90fe24d9b4ca873aeddc232062f054ee24655972
Lesedi <u22564812@tuks.co.za>
2025-06-07 23:20:52 +0200

    Added routes and commented out pages that are yet to be implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
-import UserProfile from "./pages/UserProfile"
 import LandingPage from "./pages/LandingPage";
+// import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Construction from "./pages/Construction";
+import RegisterPage from "./pages/RegisterPage";
+import Forgot from "./pages/Forgot";
+import UserProfile from "./pages/UserProfile";
+// import SearchPage from "./pages/SearchPage";
+// import NotificationsPage from "./pages/NotificationsPage";
+// import EditProfilePage from "./pages/EditProfile";
 
 function App() {
   return (
     <Routes>
       {/* Future routes: <Route path="/" element={<Landing />} /> */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/profile" element={<UserProfile />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/construction" element={<Construction />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgotpassword" element={<Forgot />} />
+      <Route path="/user-profile" element={<UserProfile />} />
+      {/* <Route path="/home" element={<HomePage />} /> */}
+      {/* <Route path="/search" element={<SearchPage />} /> */}
+      {/* <Route path="/notifications" element={<NotificationsPage />} /> */}
+      {/* <Route path="/edit-profile" element={<EditProfilePage />} /> */}
     </Routes>
   )
 }
