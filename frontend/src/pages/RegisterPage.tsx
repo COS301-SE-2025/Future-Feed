9b3b07da47d22abec8e042b09a5dfa7b349a036a
Lesedi <u22564812@tuks.co.za>
2025-06-14 12:29:59 +0200

    Fix register

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ArrowLeft } from "lucide-react";
 import logo from "../assets/Future feed transparent-Photoroom.png";
 
 interface FormData {
@@ -37,11 +38,11 @@ const RegisterPage: React.FC = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center bg-white font-['Cambay',Arial,sans-serif]">
+    <div className="flex min-h-screen flex-col items-center font-['Cambay',Arial,sans-serif] bg-gray-200">
       <div className="mt-10 flex justify-center">
         <img src={logo} alt="Future Feed Logo" className="max-w-[311px]" />
       </div>
-      <Card className="my-8 w-full max-w-[900px] rounded-[20px] border border-black bg-white px-10 py-8 shadow-[0_0_30px_#999] sm:px-12">
+      <Card className="my-8 w-full max-w-[900px] rounded-[20px] border-2 border-lime-500 bg-white px-10 py-8 shadow-[0_0_30px_#999] sm:px-12">
         <CardHeader>
           <CardTitle className="text-center text-4xl sm:text-[40px]">
             Register
@@ -50,9 +51,15 @@ const RegisterPage: React.FC = () => {
         <CardContent>
           <form className="max-h-[60vh] overflow-y-auto px-5 sm:px-12">
             <div className="mb-6">
-              <Label htmlFor="profilePic" className="mb-2 block text-left text-[23px] font-bold">
-                Profile Picture
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="profilePic" className="mb-2 block text-left text-[24px] font-bold">
+                  Username
+                </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="file"
                 id="profilePic"
@@ -62,9 +69,15 @@ const RegisterPage: React.FC = () => {
               />
             </div>
             <div className="mb-6">
-              <Label htmlFor="displayName" className="mb-2 block text-left text-[23px] font-bold">
-                Display Name
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="displayName" className="mb-2 block text-left text-[23px] font-bold">
+                    Display Name
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="text"
                 id="displayName"
@@ -76,13 +89,19 @@ const RegisterPage: React.FC = () => {
               />
             </div>
             <div className="mb-6">
-              <Label htmlFor="username" className="mb-2 block text-left text-[23px] font-bold">
-                Username
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="username" className="mb-2 block text-left text-[23px] font-bold">
+                    Username
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="text"
                 id="username"
-                placeholder="Enter your username (without @)"
+                placeholder="Enter your username"
                 required
                 value={formData.username}
                 onChange={handleChange}
@@ -90,9 +109,15 @@ const RegisterPage: React.FC = () => {
               />
             </div>
             <div className="mb-6">
-              <Label htmlFor="dob" className="mb-2 block text-left text-[23px] font-bold">
-                Date of Birth
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="dob" className="mb-2 block text-left text-[23px] font-bold">
+                    Date of Birth
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="date"
                 id="dob"
@@ -103,9 +128,15 @@ const RegisterPage: React.FC = () => {
               />
             </div>
             <div className="mb-6">
-              <Label htmlFor="email" className="mb-2 block text-left text-[23px] font-bold">
-                Email
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="email" className="mb-2 block text-left text-[23px] font-bold">
+                    Email
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="email"
                 id="email"
@@ -116,10 +147,17 @@ const RegisterPage: React.FC = () => {
                 className="h-auto rounded-[25px] border border-black bg-[#e0e0e0] p-5"
               />
             </div>
+
             <div className="mb-6">
-              <Label htmlFor="password" className="mb-2 block text-left text-[23px] font-bold">
-                Password
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="password" className="mb-2 block text-left text-[23px] font-bold">
+                    Password
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="password"
                 id="password"
@@ -130,10 +168,17 @@ const RegisterPage: React.FC = () => {
                 className="h-auto rounded-[25px] border border-black bg-[#e0e0e0] p-5"
               />
             </div>
+            
             <div className="mb-6">
-              <Label htmlFor="confirmPassword" className="mb-2 block text-left text-[23px] font-bold">
-                Confirm Password
-              </Label>
+              <div className="relative my-[15px] flex items-center justify-center text-center">
+                <div className="mr-2.5 h-px w-1/3 bg-lime-500"></div>
+                <span className="text-[0.9rem] font-bold">
+                  <Label htmlFor="confirmPassword" className="mb-2 block text-left text-[23px] font-bold">
+                    Confirm Password
+                  </Label>
+                </span>
+                <div className="ml-2.5 h-px w-1/3 bg-lime-500"></div>
+              </div>
               <Input
                 type="password"
                 id="confirmPassword"
@@ -144,16 +189,17 @@ const RegisterPage: React.FC = () => {
                 className="h-auto rounded-[25px] border border-black bg-[#e0e0e0] p-5"
               />
             </div>
+
             <div className="flex flex-col items-center justify-center py-6">
               <Button
                 type="submit"
-                className="mb-6 h-[67px] w-[186px] rounded-[25px] border border-black bg-[#ddd] text-[20px] font-bold text-black shadow-[2px_2px_4px_#888] hover:bg-[#ccc] cursor-pointer"
+                className="mb-6 h-[67px] w-[186px] rounded-[25px] border border-black bg-white text-[20px] font-bold text-black shadow-[2px_2px_4px_#888] hover:bg-gray-200 hover:shadow-[1px_1px_10px_black] hover:border-lime-500 hover:border-2 cursor-pointer"
               >
                 Register
               </Button>
               <span className="text-center">
                 Already have an account?{" "}
-                <Link to="/login" className="text-blue-600 hover:underline">
+                <Link to="/login" className="text-green-600 hover:underline">
                   Login here
                 </Link>
               </span>
