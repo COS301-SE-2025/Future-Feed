cc2c57659f5130a91b92b7ff332b8d596cd65af8
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-06-23 00:36:13 +0200

    implemented feedRule model

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/FeedRule.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/FeedRule.java
@@ -1,5 +1,23 @@
 package com.syntexsquad.futurefeed.model;
 
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+
+@Entity
 public class FeedRule {
 
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String topic;
+    private String sourceType;
+    private int percentage;
+    @ManyToOne
+    private FeedPreset feedPreset;
+    public FeedRule() {}
+    public Long getId() {
+        return id;
+    }
 }
