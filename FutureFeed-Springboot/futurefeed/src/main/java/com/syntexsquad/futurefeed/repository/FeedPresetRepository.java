e996d7e4775afe69cb7ba4ff02a830dd23dae05e
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-06-23 00:37:35 +0200

    Created and Implemented FeedPreset Repository

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/FeedPresetRepository.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/FeedPresetRepository.java
@@ -1,5 +1,9 @@
 package com.syntexsquad.futurefeed.repository;
+import com.syntexsquad.futurefeed.model.FeedPreset;
+import  com.syntexsquad.futurefeed.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public class FeedPresetRepository {
-
+public interface FeedPresetRepository extends JpaRepository<FeedPreset, Long> {
+    List<FeedPreset> findByOwner(AppUser user);
 }
