23da261a64306b56e5e6f730876d3a065e19546f
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-06-23 00:34:59 +0200

    implement FeedPreset Model

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/FeedPreset.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/model/FeedPreset.java
@@ -1,16 +1,50 @@
 package com.syntexsquad.futurefeed.model;
-import com.syntexsquad.futurefeed.model.AppUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
 @Entity
 public class FeedPreset {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
+    @Getter
     private String name;
     private boolean isDefault;
 
+    @Setter
+    @Getter
     @ManyToOne
     private AppUser owner;
 
-    @OneToMany(mappedBy = "feedPreset", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "feedPreset", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<FeedRule> rules;
+
+    public FeedPreset(String name) {
+        this.name = name;
+        this.isDefault = false;
+    }
+    public FeedPreset(String name, AppUser owner) {
+        this.name = name;
+        this.owner = owner;
+        this.isDefault = false;
+    }
+    public FeedPreset() {
+        this.isDefault = false;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+    public void setDefault(boolean isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    // Getters and setters (or use Lombok if preferred)
 }
