cf24bd72842cddb07867bcd8a26a33a093051c5d
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-06-25 03:16:50 +0200

    create BookmarkController and implemented it

---
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/Controller/BookmarkController.java
@@ -0,0 +1,42 @@
+package com.syntexsquad.futurefeed.Controller;
+import com.syntexsquad.futurefeed.dto.BookmarkDto;
+import com.syntexsquad.futurefeed.model.Bookmark;
+import com.syntexsquad.futurefeed.service.BookmarkService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookmarks")
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService = bookmarkService;
+    }
+
+    @PostMapping("/{userId}/{postId}")
+    public ResponseEntity<String> addBookmark(@PathVariable Long userId, @PathVariable Integer postId) {
+        return bookmarkService.addBookmark(userId, postId)
+                ? ResponseEntity.ok("Bookmark added.")
+                : ResponseEntity.badRequest().body("Bookmark already exists.");
+    }
+
+    @DeleteMapping("/{userId}/{postId}")
+    public ResponseEntity<String> removeBookmark(@PathVariable Long userId, @PathVariable Integer postId) {
+        return bookmarkService.removeBookmark(userId, postId)
+                ? ResponseEntity.ok("Bookmark removed.")
+                : ResponseEntity.badRequest().body("Bookmark not found.");
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<BookmarkDto>> getBookmarks(@PathVariable Long userId) {
+        return ResponseEntity.ok(bookmarkService.getUserBookmarkDtos(userId));
+    }
+
+
+
+
+}
