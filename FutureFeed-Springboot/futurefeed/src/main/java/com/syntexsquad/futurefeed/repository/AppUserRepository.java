06e5b64b2998144dda55cba9b20696772bac4ac1
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-06-26 14:00:18 +0200

    Fetch by userId

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/AppUserRepository.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/AppUserRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
     Optional<AppUser> findByUsername(String username);
     Optional<AppUser> findByEmail(String email);
+    Optional<AppUser> findById(Integer id);
     List<AppUser> findByUsernameContainingIgnoreCaseOrDisplayNameContainingIgnoreCase(String username, String displayName);
 }
