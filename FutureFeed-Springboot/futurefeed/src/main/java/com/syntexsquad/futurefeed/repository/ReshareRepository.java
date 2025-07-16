1a5faf9e22375f30f68d7b8633fa11e5b5b115b5
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-07-16 20:00:15 +0200

    Unreshare fixed

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/ReshareRepository.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/ReshareRepository.java
@@ -4,8 +4,10 @@ import com.syntexsquad.futurefeed.model.Reshare;
 import com.syntexsquad.futurefeed.model.ReshareId;
 import com.syntexsquad.futurefeed.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,8 @@ public interface ReshareRepository extends JpaRepository<Reshare, ReshareId> {
     @Query("SELECT COUNT(r) > 0 FROM Reshare r WHERE r.userId = :userId AND r.post.id = :postId")
     boolean existsByUserIdAndPostId(@Param("userId") Integer userId, @Param("postId") Integer postId);
 
+    @Modifying
+    @Transactional
     @Query("DELETE FROM Reshare r WHERE r.userId = :userId AND r.post.id = :postId")
     int deleteByUserIdAndPostId(@Param("userId") Integer userId, @Param("postId") Integer postId);
 
