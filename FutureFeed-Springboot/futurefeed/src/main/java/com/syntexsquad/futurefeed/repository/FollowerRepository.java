d4f7f803fca2e223cf2aa3ac2e8d214bc75ece3e
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-07-21 22:45:44 +0200

    create a query for findTopFollowedUsers

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/FollowerRepository.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/FollowerRepository.java
@@ -2,7 +2,9 @@ package com.syntexsquad.futurefeed.repository;
 
 import com.syntexsquad.futurefeed.model.Follower;
 import com.syntexsquad.futurefeed.model.FollowerId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +13,11 @@ public interface FollowerRepository extends JpaRepository<Follower, FollowerId> 
     void deleteByFollowerIdAndFollowedId(Integer followerId, Integer followedId);
     List<Follower> findByFollowerId(Integer followerId);
     List<Follower> findByFollowedId(Integer followedId);
+
+    @Query("SELECT u, COUNT(f.followerId) AS followerCount " +
+            "FROM Follower f JOIN AppUser u ON f.followedId = u.id " +
+            "GROUP BY u ORDER BY COUNT(f.followerId) DESC")
+    List<Object[]> findTopFollowedUsers(Pageable pageable);
+
+
 }
