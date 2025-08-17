44248956c62d1c8e450912258e6c8eaf0ee86308
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-08-17 23:26:55 +0200

    Cached reshare service

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/service/ReshareService.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/service/ReshareService.java
@@ -1,11 +1,11 @@
 package com.syntexsquad.futurefeed.service;
 
-//import com.syntexsquad.futurefeed.dto.PostReshareInfoDTO;
-//import com.syntexsquad.futurefeed.dto.*;
 import com.syntexsquad.futurefeed.model.AppUser;
 import com.syntexsquad.futurefeed.model.Reshare;
 import com.syntexsquad.futurefeed.repository.ReshareRepository;
 import com.syntexsquad.futurefeed.repository.AppUserRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -37,6 +37,7 @@ public class ReshareService {
         throw new RuntimeException("User not authenticated");
     }
 
+    @CacheEvict(value = {"reshareCount", "hasReshared", "userReshares"}, allEntries = true)
     public void resharePost(Integer postId) {
         AppUser user = getAuthenticatedUser();
         if (!reshareRepository.existsByUserIdAndPostId(user.getId(), postId)) {
@@ -48,6 +49,7 @@ public class ReshareService {
     }
 
     @Transactional
+    @CacheEvict(value = {"reshareCount", "hasReshared", "userReshares"}, allEntries = true)
     public void unresharePost(Integer postId) {
         AppUser user = getAuthenticatedUser();
         try {
@@ -57,16 +59,18 @@ public class ReshareService {
         }
     }
 
-
+    @Cacheable(value = "userReshares", key = "#root.methodName + '_' + #root.target.getAuthenticatedUser().getId()")
     public List<Reshare> getResharesByUser() {
         AppUser user = getAuthenticatedUser();
         return reshareRepository.findByUserId(user.getId());
     }
 
+    @Cacheable(value = "reshareCount", key = "#postId")
     public long getReshareCount(Integer postId) {
         return reshareRepository.countByPostId(postId);
     }
 
+    @Cacheable(value = "hasReshared", key = "#postId + '_' + #root.target.getAuthenticatedUser().getId()")
     public boolean hasUserReshared(Integer postId) {
         AppUser user = getAuthenticatedUser();
         return reshareRepository.existsByUserIdAndPostId(user.getId(), postId);
