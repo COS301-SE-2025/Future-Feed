e7a9cfb6bd4102529ba0a33136a3a92af076ff06
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-08-17 23:25:04 +0200

    Create CacheConfig.java

---
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/config/CacheConfig.java
@@ -0,0 +1,37 @@
+package com.syntexsquad.futurefeed.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager(
+                "posts", 
+                "searchUsers",
+                "users", 
+                "comments", 
+                "topics", 
+                "bots",
+                "reshareCount",
+                "hasReshared",
+                "userReshares",
+                "hasCommented",
+                "commentsByPost",
+                "topicPosts",
+                "postTopics",
+                "botPosts",
+                "userById",
+                "userByUsername",
+                "userByEmail",
+                "post", 
+                "userPosts"
+        );
+    }
+}
