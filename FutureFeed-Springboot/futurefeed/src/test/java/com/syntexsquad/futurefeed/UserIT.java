deab3f28eb59343fd74e1e3a8a31b8669d0037eb
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-08-20 08:04:18 +0200

    tests

--- FutureFeed-Springboot/futurefeed/src/test/java/com/syntexsquad/futurefeed/UserIT.java
+++ FutureFeed-Springboot/futurefeed/src/test/java/com/syntexsquad/futurefeed/UserIT.java
@@ -1,14 +1,17 @@
 package com.syntexsquad.futurefeed;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syntexsquad.futurefeed.config.S3Config;
 import com.syntexsquad.futurefeed.model.AppUser;
 import com.syntexsquad.futurefeed.repository.*;
+import com.syntexsquad.futurefeed.service.MediaService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -50,6 +53,8 @@ public class UserIT {
     @Autowired private BotPostRepository botPostRepo;
     @Autowired private BotRepository botRepo;
     @Autowired private ObjectMapper objectMapper;
+    @MockBean private S3Config s3Config;
+    @MockBean private MediaService mediaService;
 
     private AppUser testUser;
 
