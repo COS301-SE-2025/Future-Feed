d349ad541c3f7b07b40ee4d595582ddccd71d807
chuck776 <u22567659@tuks.co.za>
2025-09-09 21:19:59 +0200

    help dark mode

--- frontend/src/pages/Help.tsx
+++ frontend/src/pages/Help.tsx
@@ -28,10 +28,10 @@ const Help = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="p-6 dark:text-slate-300 min-h-screen dark:bg-black">
-      <h1 className="font-bold text-2xl text-lime-500 text-right">Help Centre</h1>
+    <div className="p-6 dark:text-slate-300 min-h-screen dark:bg-blue-950">
+      <h1 className="font-bold text-2xl dark:text-slate-200 dark:text-slate-200 text-right">Help Centre</h1>
 
-      <div className="flex border rounded-2xl justify-between items-center my-3 px-4 py-3 sticky top-0 dark:bg-black border-lime-500">
+      <div className="flex border dark:border-slate-200 rounded-2xl justify-between items-center my-3 px-4 py-3 sticky top-0 dark:bg-blue-950 ">
         {/* Breadcrumb */}
         <Breadcrumb>
           <BreadcrumbList>
@@ -60,12 +60,12 @@ const Help = () => {
       {/* split into mobile and desktop to include more videos w a cleaner UI hopefully*/ }
       <div className="flex flex-col gap-8 mt-6 mx-auto max-w-2xl">
   {/* Mobile Section */}
-  <h2 className="text-xl font-bold text-lime-500">Mobile Devices</h2>
+  <h2 className="text-xl font-bold dark:text-slate-200">Mobile Devices</h2>
   <Accordion type="single" collapsible className="w-full">
     <AccordionItem value="mobile-login">
       <AccordionTrigger>How to Log In</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={HowToLoginMobile} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -74,7 +74,7 @@ const Help = () => {
     <AccordionItem value="mobile-post">
       <AccordionTrigger>How to Create a Post</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={PostCreationHelp} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -83,7 +83,7 @@ const Help = () => {
     <AccordionItem value="mobile-follow">
       <AccordionTrigger>How to Follow Users</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={FollowHelpMobile} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -92,7 +92,7 @@ const Help = () => {
      <AccordionItem value="mobile-theme">
       <AccordionTrigger>How to Change Theme </AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={HowToChangeThemeMobile} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -101,12 +101,12 @@ const Help = () => {
   
 
   {/* Desktop Section */}
-  <h2 className="text-xl font-bold text-lime-500 mt-8">Larger Screen Devices</h2>
+  <h2 className="text-xl font-bold dark:text-slate-200 mt-8">Larger Screen Devices</h2>
   <Accordion type="single" collapsible className="w-full">
     <AccordionItem value="desktop-login">
       <AccordionTrigger>How to Log In</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={LoginHelp} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -115,7 +115,7 @@ const Help = () => {
     <AccordionItem value="desktop-post">
       <AccordionTrigger>How to Create a Post</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={PostCreationHelp} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -124,7 +124,7 @@ const Help = () => {
     <AccordionItem value="desktop-follow">
       <AccordionTrigger>How to Follow Users</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={FollowHelpDesktop} type="video/mp4" />
         </video>
       </AccordionContent>
@@ -132,7 +132,7 @@ const Help = () => {
      <AccordionItem value="-theme">
       <AccordionTrigger>How to Change Theme</AccordionTrigger>
       <AccordionContent>
-        <video controls className="w-full rounded-xl border border-lime-500">
+        <video controls className="w-full rounded-xl border ">
           <source src={HowToChangeThemeDesktop} type="video/mp4" />
         </video>
       </AccordionContent>
