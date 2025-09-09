30bc441a242a47710ff7d1bab5ce1cad781cca00
chuck776 <u22567659@tuks.co.za>
2025-09-09 21:48:21 +0200

    landing dark mode

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "dark:bg-slate-200 dark:text-black bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "dark:hover:text-slate-200 dark:hover:border-slate-200 border  dark:hover:bg-indigo-950  dark:bg-slate-200 dark:text-black bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "dark:bg-indigo-950 dark:text-slate-200 dark:border-slate-200 border bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "dark:hover:bg-slate-200 dark:hover:text-black dark:bg-indigo-950 dark:text-slate-200 dark:border-slate-200 border bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
