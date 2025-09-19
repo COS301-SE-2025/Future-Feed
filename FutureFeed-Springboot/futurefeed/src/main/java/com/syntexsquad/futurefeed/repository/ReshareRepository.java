193ca02b2c549a7dd98b6e3baeca9d6e5b8ec4ef
u21740012 <103139145+u21740012@users.noreply.github.com>
2025-09-19 08:27:47 +0200

    Update ReshareRepository.java

--- FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/ReshareRepository.java
+++ FutureFeed-Springboot/futurefeed/src/main/java/com/syntexsquad/futurefeed/repository/ReshareRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.EntityGraph;
+
 
 import java.util.List;
 
@@ -20,6 +22,7 @@ public interface ReshareRepository extends JpaRepository<Reshare, ReshareId> {
     @Query("DELETE FROM Reshare r WHERE r.userId = :userId AND r.post.id = :postId")
     int deleteByUserIdAndPostId(@Param("userId") Integer userId, @Param("postId") Integer postId);
 
+    @EntityGraph(attributePaths = {"post"})
     List<Reshare> findByUserId(Integer userId);
 
     @Query("SELECT COUNT(r) FROM Reshare r WHERE r.post.id = :postId")
