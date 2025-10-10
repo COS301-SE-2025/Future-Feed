ff26fc870e0aae46e48aa72337273181702ef2ff
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-10-10 10:54:56 +0200

    Blue for bots

--- frontend/src/components/ui/BotPost.tsx
+++ frontend/src/components/ui/BotPost.tsx
@@ -210,7 +210,7 @@ const BotPost: React.FC<PostProps> = ({
                 {topics.map((topic) => (
                   <span
                     key={topic.id}
-                    className="inline-block border dark:border-lime-400 bg-lime-300 dark:bg-lime-900 text-black dark:text-lime-200 text-xs sm:text-sm px-2 rounded-md"
+                    className="inline-block border dark:border-lime-400 bg-blue-300 dark:bg-lime-900 text-black dark:text-lime-200 text-xs sm:text-sm px-2 rounded-md"
                   >
                     {topic.name}
                   </span>
