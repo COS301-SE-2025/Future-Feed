d098ffd154fb9451eb29fbfa377c6b79e5b05934
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-10-13 12:57:02 +0200

    persnal profile UI Update

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1647,7 +1647,7 @@ const UserProfile = () => {
       <aside className="w-full lg:w-[245px] lg:ml-6 flex-shrink-0 lg:sticky lg:top-0 lg:h-screen overflow-y-auto">
         <PersonalSidebar />
       </aside>
-      <main className="flex-1 p-4 lg:pt-4 p-4 lg:p-2 lg:pl-2 min-h-screen overflow-y-auto mt-[21px]">
+      <main className="flex-1 min-h-screen overflow-y-auto mt-[21px]">
       <Card className="mb-5 ">
         <CardContent className="ml-[-10px]">
           <div className="relative">
@@ -2203,7 +2203,7 @@ const UserProfile = () => {
           </TabsContent>
         </Tabs>
       </main>
-      <aside className="w-full lg:w-[350px] lg:sticky    lg:mt-[10px] lg:top-[16px] lg:h-screen  hidden lg:block mr-6.5 ">
+      <aside className="w-full lg:w-[350px] lg:sticky lg:h-screen hidden lg:block mr-6.5 ">
           <div className="w-full lg:w-[320px] mt-5 lg:ml-7">
             <WhatsHappening />
            
