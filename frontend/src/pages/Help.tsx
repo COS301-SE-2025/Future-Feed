dbbb50508350f0a46d6d1634d7ace6c34a2f85e7
chuck776 <u22567659@tuks.co.za>
2025-10-19 22:22:15 +0200

    lint errors

--- frontend/src/pages/Help.tsx
+++ frontend/src/pages/Help.tsx
@@ -59,108 +59,30 @@ const Help = () => {
   };
 
   useEffect(() => {
-    const initializeData = async () => {
-      await fetchCurrentUser();
-    };
+  const initializeData = async () => {
+    await fetchCurrentUser();
+  };
 
-    initializeData();
-  }, []);
+  initializeData();
+}, []);
 
-  if (!currentUser) {
-    console.error("You are not logged in. Please log in.");
-    navigate("/login");
-  }
+if(!currentUser){
+  console.error("You are not logged in. Please log in.");
+  navigate("/login");
+}
+ 
 
-
-  return (
-    <div className="flex items-start future-feed:bg-black future-feed:text-lime  min-h-screen bg-ffgrey">
-      <aside className="lg:w-[245px] lg:ml-6 flex-shrink-0 lg:sticky lg:top-0 lg:h-screen overflow-y-auto">
+ return (
+    <div className="flex items-start future-feed:bg-black future-feed:text-lime  min-h-screen bg-ffgrey dark:bg-blue-950 dark:text-white">
+       <aside className="lg:w-[245px] lg:ml-6 flex-shrink-0 lg:sticky lg:top-0 lg:h-screen overflow-y-auto">
         <PersonalSidebar />
       </aside>
 
       <main className="flex-1 sm:p9 p-4  min-h-screen overflow-y-auto">
 
 
-        <div className="border-rose-gold-accent-border future-feed:border-lime future-feed:bg-card flex border justify-between items-center px-2 py-4 sticky top-0 z-10">
-          <h1 className="text-xl font-bold">Help Center</h1>
-        </div>
-        <div className="drop-shadow-xl flex flex-col gap-2 p-3 rounded-lg w-full bg-white">
-          <h2 className="text-xl future-feed:text-lime font-bold ">Mobile Devices</h2>
-          <Accordion type="single" collapsible className="w-full ">
-            <AccordionItem value="mobile-login">
-              <AccordionTrigger>How to Log In</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full rounded-xl future-feed:border-lime border ">
-                  <source src={HowToLoginMobile} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="mobile-post">
-              <AccordionTrigger>How to Create a Post</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToCreatePostsMobile} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="mobile-follow">
-              <AccordionTrigger>How to Follow Users</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToFollowMobile} type="video/mp4" />
-                </video>
-              </AccordionContent>
-
-            </AccordionItem>
-            <AccordionItem value="mobile-theme">
-              <AccordionTrigger>How to Create a Bot </AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToCreateBotsMobile} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-
-          <h2 className="text-xl future-feed:text-lime font-bold mt-8">Larger Screen Devices</h2>
-          <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="desktop-login">
-              <AccordionTrigger>How to Log In</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowtoLoginDesktop} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="desktop-post">
-              <AccordionTrigger>How to Create a Post</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToCreatePostDesktop} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="desktop-follow">
-              <AccordionTrigger>How to Follow Users</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToFollowDesktop} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="-theme">
-              <AccordionTrigger>How to Create a Bot</AccordionTrigger>
-              <AccordionContent>
-                <video controls className="w-full future-feed:border-lime rounded-xl border ">
-                  <source src={HowToCreateBotsDesktop} type="video/mp4" />
-                </video>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+        <div className="border-rose-gold-accent-border future-feed:border-lime future-feed:bg-card flex border justify-between items-center px-2 py-4 sticky top-0 dark:bg-indigo-950 border-none dark:border-slate-200  z-10">
+          <h1 className="text-xl dark:text-slate-200 font-bold">Help Center</h1>
         </div>
         {/* Videos Section */}
       {/* split into mobile and desktop to include more videos w a cleaner UI hopefully*/ }
@@ -255,18 +177,18 @@ const Help = () => {
       </main>
 
       <aside className="w-full lg:w-[350px]    lg:sticky lg:h-screen  hidden lg:block mr-6.5 ">
-        <div className="w-full lg:w-[320px] mt-5 lg:ml-7 ">
-          <WhatsHappening />
-
-        </div>
-        <div className="w-full lg:w-[320px] mt-5 lg:ml-7">
-
-          <WhoToFollow />
-        </div>
-
-      </aside>
+          <div className="w-full lg:w-[320px] mt-5 lg:ml-7 ">
+            <WhatsHappening />
+           
+          </div>
+          <div className="w-full lg:w-[320px] mt-5 lg:ml-7">
+        
+            <WhoToFollow />
+          </div>
+        
+        </aside>
     </div>
-
+  
   );
 };
 
